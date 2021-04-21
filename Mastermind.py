8469298512799c3ca26bb7b462c747e9730fcb85
Sharon Tusingizimana <Shartusi@lycksele.se>
2021-04-21 15:52:11 +0200

    skrivit meny i code

--- Mastermind.py
+++ Mastermind.py
@@ -1,5 +1,12 @@
 
 # coding: latin-1
+''' This is how this programm works, as a player you are going to start the game by guessing a four digit 
+    optional code.The user will inspect the guessed code to examine if there be any of the elements in it
+    that are also found  in the correct code, and if there be any, the user has to see if it is found in the right 
+    position and print + in the position of that correct element, otherwise the user prints out blank,
+   however if the guessed code contions any element it print - in the correct position of that element 
+   that is found in the code but allocated in an incorrect position. 
+   And it upp to the play to found what that element must be'''
 
 # Mastermind game 
 import random 
